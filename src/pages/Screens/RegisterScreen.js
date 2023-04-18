b062552bfc69cc15612d7f5b7f18171aae0fea12
Eileenpngg <eileenpng1843@gmail.com>
2023-04-18 13:17:32 +0800

    'updated register'

--- src/pages/Screens/RegisterScreen.js
+++ src/pages/Screens/RegisterScreen.js
@@ -37,7 +37,6 @@ const RegisterScreen = (props) => {
   const url = "https://dolphinswimschoolbackend.onrender.com";
 
   async function handleRegister({ data }) {
-    console.log(data);
     const response = await fetch(`${url}/api/user/create`, {
       method: "POST",
       headers: {
